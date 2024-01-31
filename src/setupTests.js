dd2f3eebeb83e5e490bdd82f45ba72158cb6e186
Cythilya Tang <cythilya@gmail.com>
2024-01-31 21:43:24 +0800

    chore: install testing framework

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import { configure as configureTestingLibrary } from '@testing-library/react';
+
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+
+configure({ adapter: new Adapter() });
+configureTestingLibrary({ testIdAttribute: 'data-test-id' });
