a093974ff9de4236adf8632fbf79340e6851a96c
Cythilya Tang <cythilya@gmail.com>
2024-01-31 23:58:27 +0800

    feat: cypress for unit test example

---
+++ src/Counter/Counter.cy.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Counter from './Counter'
+
+describe('<Counter />', () => {
+  it('should get 1 when click the increment button', () => {
+    cy.mount(<Counter />)
+    cy.get('[data-test-id="increment-button"]').click();
+    cy.get('[data-test-id="counter-value"]').should('have.text', '1');
+  })
+
+  it('should get -1 when click the decrement button', () => {
+    cy.mount(<Counter />)
+    cy.get('[data-test-id="decrement-button"]').click();
+    cy.get('[data-test-id="counter-value"]').should('have.text', '-1');
+  });
+})
