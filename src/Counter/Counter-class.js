ffaf4d905bac0f052b06e6cef05f43ed538566f8
Cythilya Tang <cythilya@gmail.com>
2024-02-26 11:09:36 +0800

    feat: add test coverage settings

--- src/Counter/Counter-class.js
+++ src/Counter/Counter-class.js
@@ -1,7 +1,10 @@
 import { Component } from 'react';
 
 class Counter extends Component {
-  state = { count: 0 };
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
 
   increment = () => {
     this.setState((prevState) => ({
