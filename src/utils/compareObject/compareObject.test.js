c93e716f453ea7dae72bf173ccb2c0a1cd9870d0
Cythilya Tang <cythilya@gmail.com>
2024-03-12 12:30:33 +0800

    feat: toBe, toEqual, toStrictEqual

--- src/utils/compareObject/compareObject.test.js
+++ src/utils/compareObject/compareObject.test.js
@@ -9,6 +9,45 @@ test('get true when compare objects by using toEqual', () => {
   expect(aObj).toEqual(bObj);
 });
 
+test('get true when compare objects by using toStrictEqual', () => {
+  expect(aObj).toStrictEqual(bObj);
+});
+
+test('check array sparseness', () => {
+  expect([undefined, undefined, undefined, undefined, undefined, 1]).toEqual([
+    ,
+    ,
+    ,
+    ,
+    ,
+    1,
+  ]);
+  expect([
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    1,
+  ]).not.toStrictEqual([, , , , , 1]);
+});
+
+test('check undefined in object', () => {
+  expect({ a: undefined, b: 2 }).toEqual({ b: 2 });
+  expect({ a: undefined, b: 2 }).not.toStrictEqual({ b: 2 });
+});
+
+test('check object types', () => {
+  class Obj {
+    constructor() {
+      this.hello = 'world';
+    }
+  }
+
+  expect(new Obj()).toEqual({ hello: 'world' });
+  expect(new Obj()).not.toStrictEqual({ hello: 'world' });
+});
+
 test.skip('get propery error message when using toBeNull', () => {
   expect(1).toBe(null);
   // expect(1).toEqual(null);
