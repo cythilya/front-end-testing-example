15fd2b94c825f9c090f6f0163f7f425f256134bf
Summer Tang <cythilya@gmail.com>
2024-08-08 11:05:35 +0800

    feat: reduce re-render by memorizing the calculated result

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-import ItemList from './ItemList';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ItemList />
         {/* <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
